b59bd10537e96c2c45fc02a37390edb7add93b19
Erdum <erdumadnan@gmail.com>
2022-12-26 15:15:37 +0500

    Update type casting removed

--- core/response.php
+++ core/response.php
@@ -1,7 +1,7 @@
 <?php 
 
-function send_response($payload = array(), $status_code = 200) {
+function send_response($payload, $status_code = 200) {
   header('Content-Type: application/json');
   http_response_code($status_code);
-  exit(json_encode((object) $payload));
+  exit(json_encode($payload));
 };
