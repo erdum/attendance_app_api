29021c6de421dfd9f002b3914a59f645e9b9f7fa
Erdum <erdumadnan@gmail.com>
2022-12-26 11:45:34 +0500

    Update insert check in method complete

--- core/db.php
+++ core/db.php
@@ -46,6 +46,16 @@ function insertCheckIn(
         :location
     );
 
-    $query->bindValue()
-SQL;
+    $stmt = $db->prepare($query);
+
+    $stmt->bindParam(':uid', $uid, SQLITE3_INTEGER);
+    $stmt->bindParam(':name', $name, SQLITE3_TEXT);
+    $stmt->bindParam(':email', $email, SQLITE3_TEXT);
+    $stmt->bindParam(':date', $check_in_date, SQLITE3_TEXT);
+    $stmt->bindParam(':time', $check_in_time, SQLITE3_TEXT);
+    $stmt->bindParam(':cordinates', $check_in_cordinates, SQLITE3_TEXT);
+    $stmt->bindParam(':location', $check_in_location, SQLITE3_TEXT);
+
+    $stmt->execute();
+    $stmt->reset();
 }
