c770d2ca60e8709873572b4cbc72fca013e78f92
Erdum <erdumadnan@gmail.com>
2022-12-26 12:26:10 +0500

    Update composer removed

--- index.php
+++ index.php
@@ -1,13 +1,15 @@
 <?php
 
-include 'vendor/autoload.php';
-
 require_once __DIR__.'/core/router.php';
 require_once __DIR__.'/core/response.php';
 require_once __DIR__.'/core/db.php';
 
 get('/attendances', function() {
   send_response(getAllAttendances());
+});
+
+post('/attendance', function() {
+  exit(print_r($_POST));
 });
 
 any('/404', '404.php');
