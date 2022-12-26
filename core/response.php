4ecbdc37864e5f62e2460f4206fdd15dfd19009c
Erdum <erdumadnan@gmail.com>
2022-12-26 15:21:32 +0500

    Update converting array to object before sending

--- core/response.php
+++ core/response.php
@@ -3,5 +3,5 @@
 function send_response($payload, $status_code = 200) {
   header('Content-Type: application/json');
   http_response_code($status_code);
-  exit(json_encode($payload));
+  exit(json_encode((object) $payload));
 };
