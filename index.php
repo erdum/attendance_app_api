458dfa5819b6981f0f00441968b5369a4eae80b1
Erdum <erdumadnan@gmail.com>
2022-12-27 23:41:33 +0500

    Update checkout route added

--- index.php
+++ index.php
@@ -9,8 +9,9 @@ $config = array(
 
 $attendanceController = new AttendanceController($config['db_path']);
 
-get('attendance/today/$date', array($attendanceController, 'getAttendanceByDay'));
+get('/attendance/today/$date', array($attendanceController, 'getAttendanceByDay'));
 get('/attendance/csv/$year/$month', array($attendanceController, 'getMonthlyAttendanceCSV'));
+put('/attendance/checkout', array($attendanceController, 'markCheckout'));
 put('/attendance', array($attendanceController, 'markAttendance'));
 
 any('/404', '404.php');
