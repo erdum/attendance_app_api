676604d7eceadcb4c49a28258c4410b3bb3c5003
Erdum <erdumadnan@gmail.com>
2022-12-27 16:40:02 +0500

    Update date and time column types modified to integer

--- database/schema.sql
+++ database/schema.sql
@@ -1,15 +1,15 @@
 CREATE TABLE attendance (
     id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
-    uid text NOT NULL,
-    name text NOT NULL,
-    email text NOT NULL,
-    check_in_date text NOT NULL,
-    check_in_time text NOT NULL,
-    check_out_date text,
-    check_out_time text,
-    check_in_coordinates text NOT NULL,
-    check_out_coordinates text,
-    check_in_location text NOT NULL
+    uid TEXT NOT NULL,
+    name TEXT NOT NULL,
+    email TEXT NOT NULL,
+    check_in_date INTEGER NOT NULL,
+    check_in_time INTEGER NOT NULL,
+    check_out_date INTEGER,
+    check_out_time INTEGER,
+    check_in_coordinates TEXT NOT NULL,
+    check_out_coordinates TEXT,
+    check_in_location TEXT NOT NULL
 );
 
 CREATE INDEX uid ON attendance (uid);
