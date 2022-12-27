ed8327670b6eb9d6bf76858496ca051b79a5aa19
Erdum <erdumadnan@gmail.com>
2022-12-27 21:22:14 +0500

    Update Attendance model class implemented

--- core/db.php
+++ core/db.php
@@ -1,87 +1,88 @@
 <?php
 
-$db = new SQLite3('database/database.db');
+class AttendanceModel {
 
-function getAttendanceByMonth($date) {
+    public function __construct($db_file_path) {
+        $this->db = new SQLite3($db_file_path);
+    }
 
-    global $db;
-    $query = <<<SQL
-    select * from attendance where check_in_date >= :from_date and check_in_date < :to_date ;
-SQL;
+    public function byDay($date) {
+        
+        $query = <<<SQL
+        select * from attendance where check_in_date = :date;
+        SQL;
 
-    $stmt = $db->prepare($query);
+        $stmt = $this->db->prepare($query);
 
-    $from_date = (int) date('Ymd', strtotime($date));
-    $to_date = (int) date("Ymd", strtotime('+1 month', strtotime($date)));
+        $day = (int) date('Ymd', strtotime($date));
+        $stmt->bindParam(':date', $day, SQLITE3_INTEGER);
 
-    $stmt->bindParam(':from_date', $from_date, SQLITE3_INTEGER);
-    $stmt->bindParam(':to_date', $to_date, SQLITE3_INTEGER);
+        $result = $stmt->execute();
+        $stmt->reset();
 
-    $result = $stmt->execute();
-    $stmt->reset();
+        return $result;
+    }
 
-    return $result;    
-}
+    public function byMonth($date) {
 
-function insertCheckIn(
-    $uid,
-    $name,
-    $email,
-    $check_in_date,
-    $check_in_time,
-    $check_in_coordinates,
-    $check_in_location
-) {
+        $query = <<<SQL
+        select * from attendance where check_in_date >= :from_date and check_in_date < :to_date;
+        SQL;
 
-    global $db;
-    $query = <<<SQL
-    insert into attendance (
-        uid,
-        name,
-        email,
-        check_in_date,
-        check_in_time,
-        check_in_coordinates,
-        check_in_location
-    ) values (
-        :uid,
-        :name,
-        :email,
-        :date,
-        :time,
-        :coordinates,
-        :location
-    );
-SQL;
+        $stmt = $this->db->prepare($query);
 
-    $stmt = $db->prepare($query);
+        $from_date = (int) date('Ymd', strtotime($date));
+        $to_date = (int) date("Ymd", strtotime('+1 month', strtotime($date)));
 
-    $stmt->bindParam(':uid', $uid, SQLITE3_TEXT);
-    $stmt->bindParam(':name', $name, SQLITE3_TEXT);
-    $stmt->bindParam(':email', $email, SQLITE3_TEXT);
-    $stmt->bindParam(':date', date('Ymd', strtotime($check_in_date)), SQLITE3_INTEGER);
-    $stmt->bindParam(':time', $check_in_time, SQLITE3_INTEGER);
-    $stmt->bindParam(':coordinates', $check_in_coordinates, SQLITE3_TEXT);
-    $stmt->bindParam(':location', $check_in_location, SQLITE3_TEXT);
+        $stmt->bindParam(':from_date', $from_date, SQLITE3_INTEGER);
+        $stmt->bindParam(':to_date', $to_date, SQLITE3_INTEGER);
 
-    $stmt->execute();
-    $stmt->reset();
-}
+        $result = $stmt->execute();
+        $stmt->reset();
 
-function getUserAttendanceByDate($uid, $date) {
+        return $result;
+    }
 
-    global $db;
-    $query = <<<SQL
-    select * from attendance where uid = :uid and check_in_date = :date;
-SQL;
+    public function insertAttendance(
+        $uid,
+        $name,
+        $email,
+        $check_in_date,
+        $check_in_time,
+        $check_in_coordinates,
+        $check_in_location
+    ) {
+        $query = <<<SQL
+        insert into attendance (
+            uid,
+            name,
+            email,
+            check_in_date,
+            check_in_time,
+            check_in_coordinates,
+            check_in_location
+        ) values (
+            :uid,
+            :name,
+            :email,
+            :date,
+            :time,
+            :coordinates,
+            :location
+        );
+        SQL;
 
-    $stmt = $db->prepare($query);
+        $stmt = $this->db->prepare($query);
 
-    $stmt->bindParam(':uid', $uid, SQLITE3_TEXT);
-    $stmt->bindParam(':date', $date, SQLITE3_TEXT);
+        $stmt->bindParam(':uid', $uid, SQLITE3_TEXT);
+        $stmt->bindParam(':name', $name, SQLITE3_TEXT);
+        $stmt->bindParam(':email', $email, SQLITE3_TEXT);
+        $stmt->bindParam(':date', date('Ymd', strtotime($check_in_date)), SQLITE3_INTEGER);
+        $stmt->bindParam(':time', $check_in_time, SQLITE3_INTEGER);
+        $stmt->bindParam(':coordinates', $check_in_coordinates, SQLITE3_TEXT);
+        $stmt->bindParam(':location', $check_in_location, SQLITE3_TEXT);
 
-    $result = $stmt->execute();
-    $stmt->reset();
-
-    return $result->fetchArray(SQLITE3_ASSOC);
+        $stmt->execute();
+        $stmt->reset();
+    }
 }
