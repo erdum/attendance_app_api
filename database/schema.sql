383c339801e80d697c36f037e97312ee764847bf
Erdum <erdumadnan@gmail.com>
2022-12-27 10:03:52 +0500

    Update schema updated primary key auto increment added

--- database/schema.sql
+++ database/schema.sql
@@ -1,13 +1,13 @@
 CREATE TABLE attendance (
-    id INTEGER PRIMARY KEY,
-    uid text,
-    name text,
-    email text,
-    check_in_date text,
-    check_in_time text,
+    id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+    uid text NOT NULL,
+    name text NOT NULL,
+    email text NOT NULL,
+    check_in_date text NOT NULL,
+    check_in_time text NOT NULL,
     check_out_date text,
     check_out_time text,
-    check_in_coordinates text,
+    check_in_coordinates text NOT NULL,
     check_out_coordinates text,
-    check_in_location text
+    check_in_location text NOT NULL
 );
