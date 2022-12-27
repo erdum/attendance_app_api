8dae2b3715a84728f079fd53d46b767faebe4dd7
Erdum <erdumadnan@gmail.com>
2022-12-27 09:51:43 +0500

    Update column spelling corrected

--- database/schema.sql
+++ database/schema.sql
@@ -7,7 +7,7 @@ CREATE TABLE attendance (
     check_in_time text,
     check_out_date text,
     check_out_time text,
-    check_in_cordinates text,
+    check_in_coordinates text,
     check_out_coordinates text,
     check_in_location text
 );
