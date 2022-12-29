19438e43069c00c023f510ac9c432f8624f0a491
Erdum <erdumadnan@gmail.com>
2022-12-30 00:40:26 +0500

    Update CORS handling

--- core/response.php
+++ core/response.php
@@ -1,6 +1,7 @@
 <?php 
 
 function send_response($payload, $status_code = 200) {
+  header('Access-Control-Allow-Origin: *');
   header('Content-Type: application/json');
   header("HTTP/1.0 $status_code");
   exit(json_encode((object) $payload));
--- index.php
+++ index.php
@@ -3,6 +3,16 @@
 require_once __DIR__.'/core/router.php';
 require_once __DIR__.'/controllers/AttendanceController.php';
 
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+  header('Access-Control-Allow-Origin: *');
+  header('Access-Control-Allow-Methods: POST, GET, OPTIONS');
+  header('Access-Control-Allow-Headers: apikey, Content-Type');
+  header('Access-Control-Max-Age: 300');
+  header('Content-Length: 0');
+  header('Content-Type: application/json');
+  die();
+}
+
 $config = array(
   'db_path' => __DIR__.'/database/database.db'
 );
