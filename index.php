be6a4ec1455534853e9ade4eecaa277fe98559fe
Erdum <erdumadnan@gmail.com>
2022-12-29 15:28:40 +0500

    Update method changed

--- index.php
+++ index.php
@@ -11,7 +11,7 @@ $attendanceController = new AttendanceController($config['db_path']);
 
 get('/attendance/today/$date', array($attendanceController, 'getAttendanceByDay'));
 get('/attendance/csv/$year/$month', array($attendanceController, 'getMonthlyAttendanceCSV'));
-put('/attendance/checkout', array($attendanceController, 'markCheckout'));
-put('/attendance', array($attendanceController, 'markAttendance'));
+post('/attendance/checkout', array($attendanceController, 'markCheckout'));
+post('/attendance', array($attendanceController, 'markAttendance'));
 
 any('/404', '404.php');
