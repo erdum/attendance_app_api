f8597f790d21205e20e4bf860cf15478ac667b64
Erdum <erdumadnan@gmail.com>
2022-12-30 11:54:06 +0500

    Update global time zone set to utc

--- index.php
+++ index.php
@@ -3,6 +3,8 @@
 require_once __DIR__.'/core/router.php';
 require_once __DIR__.'/controllers/AttendanceController.php';
 
+date_default_timezone_set('UTC');
+
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
   header('Access-Control-Allow-Origin: *');
   header('Access-Control-Allow-Methods: POST, GET, OPTIONS');
